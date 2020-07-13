9f126fb0b56873853f01bf6026218ca07feaed29
Timothy Lalli <trlalli92@gmail.com>
2020-07-13 14:11:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,18 @@
-def badge_maker
+def badge_maker("conference_speakers")
   conference_speakers[Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 end
 
+
 def batch_badge_creator
+  
+end
+
 
 def assign_rooms
+
+end
+
+
+def printer
+  
+end
