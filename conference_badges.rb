c5d23b3328a9f1e47b1d9b4c26fa58e1e54d9ca4
Timothy Lalli <trlalli92@gmail.com>
2020-07-13 14:26:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,8 @@
 def badge_maker(name)
   name = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
   badge_maker.each do |name| 
-  puts "Hello, my name is #{name}."
+    puts "Hello, my name is #{name}."
+  end
 end
 
 
