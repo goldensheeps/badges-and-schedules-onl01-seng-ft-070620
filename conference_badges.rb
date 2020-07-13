91824d2c57026dab81eb4dd808cdc2bb38454919
Timothy Lalli <trlalli92@gmail.com>
2020-07-13 15:17:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ end
 def assign_rooms(speakers)
   new_array = []
  speakers.each_with_index do |name, index|
-    new_array <<  "Hello, #{name}! You'll be assigned to room #{room}!"
+    new_array <<  "Hello, #{name}! You'll be assigned to room #{++index}!"
   end
 end
 
